92b477108fdf7288c46c9650b29efe90b6545c73
Leo Metzner <leometzner@MacBook-Pro-von-Leo-2.local>
2025-11-03 11:59:26 +0200

    submission

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,9 @@ public class FVCalc {
 		double currentValue = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
-		double futureValue = currentValue * Math.pow((1 + rate/100), years);
-		System.out.println("after "+years+ " years, $"+currentValue+" saved at "+rate+" will yield $" + (int)futureValue);
+		double futureValue = currentValue * Math.pow((1 + rate / 100), years);
+		int currentValueint = (int) currentValue;
+		System.out.println("After "+years+ " years, $"+currentValueint+" saved at "+rate+" will yield $" + (int)futureValue);
 
 
 		
--- TimeFormat.java
+++ TimeFormat.java
@@ -13,10 +13,9 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
-		if (hours == 0) {
-			hours = 12;
-			System.out.println(hours + ":" + String.format("%02d", minutes) + " AM");
-		} else if (10<hours && hours < 12) {
+		if (hours == 0) {;
+			System.out.println(hours + "0:" + String.format("%02d", minutes) + " AM");
+		} else if (10 <= hours && hours < 12) {
 			System.out.println(hours + ":" + String.format("%02d", minutes) + " AM");
 		}	else if (hours < 10) {
 			System.out.println("0" +hours + ":" + String.format("%02d", minutes) + " AM");
