9bfa36e542f12257a35494553a8157766f56ef61
Leo Metzner <leometzner@MacBook-Pro-von-Leo-2.local>
2025-11-03 11:33:23 +0200

    submission

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int totalBill = Integer.parseInt(args[3]);
+		double splitBill = totalBill / 3;
+		Math.ceil(splitBill);
+		System.out.println("pay " + splitBill + " each"); 
 	}
 }
